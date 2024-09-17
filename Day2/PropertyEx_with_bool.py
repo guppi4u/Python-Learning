76409dbbb9fbe47f37876f059de7fddb76960183
guppi <guppi4u@outlook.com>
2024-09-17 16:58:54 +1000

    Program has logical error

--- Day2/PropertyEx_with_bool.py
+++ Day2/PropertyEx_with_bool.py
@@ -6,12 +6,18 @@ user_somke = input("Do you smoke (y/n) ?")
 user_pet = input("Do you own a pet (y/n) ?")
 
 if (user_student == "y" and user_somke == "n" and user_pet == "n"):
-    print("Property avaiable for rent")
+    print("Property avaiable for rent 1")
 
 elif (user_student == "y" and user_somke == "y" or user_pet == "n"):
-    print("Property avaiable for rent")
+    print("Property avaiable for rent 2")
+
 elif (user_student == "n" and user_somke == "y" and user_pet == "y"):
-    print("Property avaiable not for rent")
+    print("Property not avaiable for rent")
+
+elif (user_student == "y" and user_somke == "y" and user_pet == "y"):
+    print("Property not avaiable for rent")
 
 
 print("Program finished !!!")
+
+# this program has logic problem , will upload updated one later
