2035e27536f8b1362bb7fd48803670cdb94a5402
guppi4u <122578051+guppi4u@users.noreply.github.com>
2025-03-02 02:41:23 +0000

    Adding power of number program

---
+++ misc/powerofnum.py
@@ -0,0 +1,7 @@
+# to chech the power of given number in given range 
+
+power =1 # initializing vairable power with value 1 
+
+for expo in range(16):
+    print("2 to the power of",expo,"is",power)
+    power *=2
