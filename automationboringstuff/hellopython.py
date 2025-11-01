45949b82c56f95021972facbb96969d67b4a6684
guppi4u <122578051+guppi4u@users.noreply.github.com>
2025-11-01 00:13:09 +0000

    Adding automationboringstuff with python

---
+++ automationboringstuff/hellopython.py
@@ -0,0 +1,12 @@
+''' this will create hello python message 
+'''
+
+def hello_python(): # function declaration 
+    print('Hello python')
+
+
+
+# function call 
+
+if __name__ == "__main__":
+    hello_python()
