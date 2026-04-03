ceb48c66f3576baaffc3cf69c8be0c68606efd6b
guppi4u <122578051+guppi4u@users.noreply.github.com>
2026-04-03 20:54:14 +0000

    Updated task5.py in Day-2

--- python-genai/Day-2/task5.py
+++ python-genai/Day-2/task5.py
@@ -4,16 +4,31 @@
 def loop_practice():
     count_even=0
     count_odd=0
+    sum=0
+    product=1
     for i in range(1,20):
+        sum+=i
+        product*=i
+       
         if i%2==0:
             print(f'Even number :{i}')
-            count_even+i
-            print(count_even)
-        if i%2 ==1:
+            count_even+=1
+            
+        else:
             print(f'Odd number :{i}')
-            count_odd+i
-            print(count_odd)
+            count_odd+=1
 
+    print(f'No of even number :{count_even}')
+    print(f'No of odd number :{count_odd}')
+    print(f'Sum of all the numbers :{sum}')
+    print(f'Product of all the numbers :{product}')
+
+    print("*************************************")
+
+    for i,element in enumerate(range(1,20)):
+        print()
+        print(f'{element} is at index {i}')
+        print()
 
 
 
